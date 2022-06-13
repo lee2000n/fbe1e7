3214a23574e95b318b86234ca74f26631455db79
lee2000n <lee2000n@hotmail.com>
2022-06-13 16:03:16 -0400

    Update posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { Post, UserPost } = require('../db/models');
-
+const Sequelize = require('sequelize');
 const router = express.Router();
-
+const update=require('../api/updateController');
+const {in: opIn} = Sequelize.Op;
 /**
  * Create a new blog post
  * req.body is expected to contain {text: required(string), tags: optional(Array<string>)}
@@ -40,5 +41,59 @@ router.post('/', async (req, res, next) => {
     next(error);
   }
 });
+
+//Part 1: Fetching Blog Posts
+
+router.get('/api/posts', async (req, res, next) => {
+  try {
+    const error = new Error("An error message");
+    const {authorIds} = req.query;
+    if(!authorIds){
+      return res.status(400).send({
+        message: error
+     });
+    }
+   const id_list = authorIds.split(',');
+    const users_post =  UserPost.findAll({
+      include: [
+        {
+          where: {
+            userId: {
+              [opIn]:id_list
+            },
+            
+          },
+        },
+      ],
+    });
+
+    const postIds = users_post.map(item => item.postId);
+
+    const postList = Post.findAll({
+      include: [
+        {
+          where: {
+            id: {
+              [opIn]:postIds
+            },
+            order: [
+              ['id', 'ASC'],
+              ['popularity', 'ASC'],
+          ],
+          },
+        },
+      ],
+    });
+
+
+    res.json({ post:postList });
+  } catch (error) {
+    next(error);
+  }
+//part 2 Updating a Blog Post
+router.put(`/api/posts/${UserPost.postId}`, update);
+});
+
+
 
 module.exports = router;
