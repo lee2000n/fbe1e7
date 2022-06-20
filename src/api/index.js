0ca9dad3c83d23fc146f8f73f596aefed3629112
lee2000n <lee2000n@hotmail.com>
2022-06-20 14:03:18 -0400

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -7,9 +7,9 @@ const router = express.Router();
 router.use('/', auth);
 
 router.use('/posts', posts);
-//these are the implementations for using middlewares to check if user identity and permssion
-//router.use('/',[middlewares.auth, middlewares.isAdmin], auth);
-//router.use('/',[middlewares.auth, middlewares.isAdmin], posts);
-//router.use('/',[middlewares.auth, middlewares.isUser], posts);
+//these are the implementations for using middlewares to check user identity and permssion
+router.use('/',[middlewares.auth, middlewares.isAdmin], auth);
+router.use('/',[middlewares.auth, middlewares.isAdmin], posts);
+router.use('/',[middlewares.auth, middlewares.isUser], posts);
 
 module.exports = router;
