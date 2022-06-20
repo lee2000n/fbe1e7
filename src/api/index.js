8f59b78f5a3fa25b54d9f109b6251d6923e818b1
lee2000n <lee2000n@hotmail.com>
2022-06-20 13:37:45 -0400

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const posts = require('./posts');
 const auth = require('./auth');
-
+const { middlewares } = require("../middlewares");
 const router = express.Router();
 
-router.use('/', auth);
-
-router.use('/posts', posts);
+router.use('/',[middlewares.auth, middlewares.isAdmin], auth);
+router.use('/',[middlewares.auth, middlewares.isAdmin], posts);
+router.use('/',[middlewares.auth, middlewares.isUser], posts);
 
 module.exports = router;
